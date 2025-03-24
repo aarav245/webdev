e84a4ec61f5df0e3da76d98d7dd129f3bf5a8275
aarav245 <aaravmisra24@gmail.com>
2025-03-23 19:07:56 -0700

    script.js

--- web1hw/script.js
+++ web1hw/script.js
@@ -4,4 +4,17 @@ window.onload = screensize;
 function screensize(){
     Width = window.innerWidth;
     document.getElementById("size").innerHTML = "Width:"+Width+"px";
+    let textchange=document.getElementById("textchange")
+    if (Width < 576){
+        textchange.innerHTML= "Tony Stark is the wealthy son of industrialist and weapons manufacturer Howard Stark and his wife, Maria"
+    }
+    else if (Width >= 576 && Width<768){
+        textchange.innerHTML="Natasha Romanoff, known as Black Widow, was a Russian spy trained in the Red Room, a KGB academy, and later became a founding member of the Avengers. ";
+    }
+    else if (Width >= 768 && Width<992){
+        textchange.innerHTML= "Captain America's backstory centers around Steve Rogers, a frail man rejected for military service in WWII, who becomes a super-soldier after receiving the Super-Soldier Serum.";
+    }
+    else if (Width>=992 && Width<1920){
+        textchange.innerHTML= "Bruce Banner, a brilliant nuclear physicist and biochemist, became the Hulk after being exposed to gamma radiation during an experiment.";
+    }
 }
