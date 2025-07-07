0394dcac5c7d66826fb17c031a9983c2fb70fe17
aarav245 <aaravmisra24@gmail.com>
2025-07-06 19:19:56 -0700

    Add files via upload

---
+++ movierec/script.js
@@ -0,0 +1,43 @@
+class movie{
+    constructor(Title,Director,Year){
+        this.Title=Title
+        this.Director=Director
+        this.Year=Year
+    }
+}
+//array for default movie(s)
+const defaultmovies=[
+    {
+        Title: "Iron Man",
+        Director: "Jon Favreau",
+        Year: "2008"
+    },
+    {
+        Title: "Harry Potter and the Sorcerer's Stone",
+        Director: "Chris Columbus",
+        Year: "2001"
+    },
+    {
+        Title: "Up",
+        Director: "Pete Docter",
+        Year:"2009"
+    }
+]
+class ui{
+    //static method to add a movie
+    static addmovie = (movie) => {
+        const list = document.getElementById("movie-list")
+        const row = document.createElement("tr")
+        //setting rows innerHTML with movie data
+        row.innerHTML = `
+        <td>${movie.Title}</td>
+        <td>${movie.Director}</td>
+        <td>${movie.Year}</td>
+        `
+        list.appendChild(row)
+    }
+    static displaymovie = () => {
+        defaultmovies.forEach(movie=>uiEvent.addmovie(movie))
+    }
+}
+ui.displaymovie()
